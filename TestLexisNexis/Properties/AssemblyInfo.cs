33a8727ef8d647a34cd3f30f91daa9e32227b3ea
swethareddy <swethareddy02@gmail.com>
2020-02-02 19:47:43 -0500

    Add test project for LexisNexis

---
+++ TestLexisNexis/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestLexisNexis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestLexisNexis")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43f9e2d2-3e74-4624-ad75-dfdf7d4dfac8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestLexisNexis/TestLexisNexisInGoogle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+/*1.       Open Google search engine
+2.       Search from “LexisNexis”
+3.       Open the third link in the search result  
+4.       Return the page title
+5.       Upload your solution to a GIT repository and send the link back to us.
+*/
+
+namespace TestLexisNexis
+{
+    [TestClass]
+    public class TestLexisNexisInGoogle
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            var siteUrl = "https:\\www.google.com";
+
+            //create chrome driver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(siteUrl);            
+        }
+
+        [TestMethod]
+        public void GetThirdLinkTitleForLexisNexisInGoogle()
+        {
+            
+            var searchString = "LexisNexis";
+
+            driver.FindElement(By.Name("q")).SendKeys(searchString);
+
+            //wait for the autocomplete list
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.FindElement(By.XPath("//ul[@class='erkvQe']/li[1]//span[text()='lexisnexis']")));
+
+            driver.FindElement(By.XPath("//ul[@class='erkvQe']/li[1]//span[text()='lexisnexis']")).Click();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.FindElement(By.XPath("//a/h3")));
+
+            var link = driver.FindElements(By.XPath("//a/h3"));
+
+            link[2].Click(); //clicking third link as per the test
+                        
+            Console.WriteLine(driver.Title);
+            
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
